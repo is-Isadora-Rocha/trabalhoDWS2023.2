14505b3db16bc1bc705407cfda572a052e6fc389
7rik <bezerra.erik2003@gmail.com>
2023-09-18 20:35:12 -0300

    Criado entidades do sistema

---
+++ src/main/java/entities/Carros.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Carros {
+	
+	private String modelo;
+	private String marca;
+	private Integer anoFabricacao;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Integer getAnoFabricacao() {
+		return anoFabricacao;
+	}
+	public void setAnoFabricacao(Integer anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+	
+	
+}
