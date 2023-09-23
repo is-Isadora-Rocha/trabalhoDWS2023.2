0934b35741b00fa69f7dfd38cddc4754c2d19c75
is-Isadora-Rocha <is.isadora.rocha@gmail..com>
2023-09-22 21:52:22 -0300

    ArrayList jogo impar-par

--- src/main/java/servlets/JogoServlet.java
+++ src/main/java/servlets/JogoServlet.java
@@ -2,6 +2,8 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,32 +11,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/jogServlet")
+@WebServlet(urlPatterns ="/jogServlet")
 public class JogoServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		int num1 = Integer.parseInt(req.getParameter("numero1"));
-		int num2 = Integer.parseInt(req.getParameter("numero2"));
-		int num3 = Integer.parseInt(req.getParameter("numero3"));
-		int num4 = Integer.parseInt(req.getParameter("numero4"));
-		int num5 = Integer.parseInt(req.getParameter("numero5"));
+		ArrayList<Integer> pares = new ArrayList<>();
+		ArrayList<Integer> impares = new ArrayList<>();
 		
+		for (int i = 1; i <= 5; i++) {
+			int num = Integer.parseInt(req.getParameter("numero" + i));
+			if (num % 2 == 0) {
+				pares.add(num);
+			} else {
+				impares.add(num);
+			}
+		}
 		
-		Integer[] parOuImpar = new Integer[5];
-		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>Jogo Ímpar-Par!</title>");
+		out.println("<meta charset=\\\"UTF-8\\\"></head>");
+		out.println("<body>");
+		out.println("<p>Aqui estão os numeros pares: </p>");
+		for (Integer num : pares) {
+			out.print("<p>" + num + "</p>");
+		}
+		out.println("<p>Aqui estão os numeros ímpares: </p>");
+		for (Integer num : impares) {
+			out.print("<p>" + num + "</p>");
+		}
+		out.println("<a href=\"index.html\">Página Principal</a>");
+        out.println("</body>");
+        out.println("</html>");
 	
-		
-		PrintWriter writer = resp.getWriter();
-		writer.write("<html>");
-		writer.write("<head><title>Jogo Ímpar-Par!</title>");
-		writer.write("<meta charset=\\\"UTF-8\\\"></head>");
-		writer.write("<body>");
-		writer.write("<li>Número 1: " + num1 + "</li>");
-	
-		writer.write("<a href=\"index.html\">Página Principal</a>");
-		writer.write("</body>");
-		writer.write("</html>");
 	}
 }
+
